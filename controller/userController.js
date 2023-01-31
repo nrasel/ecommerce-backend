71a4631c718500fff3c83356648063c2a7cc728a
nrasel <nrasel452@gmail.com>
2023-02-01 01:04:43 +0600

    add controller

--- controller/userController.js
+++ controller/userController.js
@@ -3,6 +3,7 @@ const userModel = require("../models/userModel");
 module.exports.createUser = async (req, res) => {
   const email = req.body.email;
 
+  //   find user
   const findUser = await userModel.findOne({ email });
   if (!findUser) {
     // create a new user
