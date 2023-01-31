7833cc4ee10b320e9c4cc07411d0dbc62a2deb3e
nrasel <nrasel452@gmail.com>
2023-02-01 01:06:54 +0600

    add something

--- controller/userController.js
+++ controller/userController.js
@@ -3,12 +3,13 @@ const userModel = require("../models/userModel");
 module.exports.createUser = async (req, res) => {
   const email = req.body.email;
 
-  //   find user
+  // find user
   const findUser = await userModel.findOne({ email });
   if (!findUser) {
     // create a new user
     const newUser = userModel.create(req.body);
     res.json(newUser);
+    const find
   } else {
     res.json({
       msg: "User Already Exists",
