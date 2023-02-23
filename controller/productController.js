cf0390b7612a5d42b2605be05e52823fc41c10a2
nrasel <nrasel452@gmail.com>
2023-02-24 01:29:41 +0600

    get a product

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,34 @@
 const asyncHandler = require("express-async-handler");
+const productModels = require("../models/productModels");
+
+// create products
 module.exports.createProduct = asyncHandler(async (req, res) => {
-  res.json({
-    message: "Hey it's product routes",
-  });
+  try {
+    const newProduct = await productModels.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+    console.log(error);
+  }
+});
+
+// get a product
+module.exports.getaProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const findProducts = await productModels.findById(id);
+    res.json(findProducts);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// get all products
+module.exports.getAllProducts = asyncHandler(async (req, res) => {
+  try {
+    const findAllProducts = await productModels.find({});
+    res.json(findAllProducts);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,13 @@
-const { createProduct } = require("../controller/productController");
+const {
+  createProduct,
+  getaProduct,
+  getAllProducts,
+} = require("../controller/productController");
 
 const router = require("express").Router();
 
 router.post("/", createProduct);
+router.get("/:id", getaProduct);
+router.get("/", getAllProducts);
 
 module.exports = router;
